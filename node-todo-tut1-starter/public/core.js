0aa188bc25821ee16da9cfab55f2bbb6e0fdae74
Hirmuli <omena_n@hotmail.com>
2014-09-30 11:52:40 +0300

    todo päivitetty
    todo.js tiedoston päivitys

--- node-todo-tut1-starter/public/core.js
+++ node-todo-tut1-starter/public/core.js
@@ -2,18 +2,21 @@ var scotchTodo = angular.module('scotchTodo', []);
 
 function mainController($scope, $http) {
 	$scope.formData = {};
+	var kohteet = {};
 
 	// when landing on the page, get all todos and show them
-	$http.get('/api/todos')
+	$http.get('/api/citys')
 		.success(function(data) {
 			$scope.todos = data;
+			kohteet = data;
+			console.log(data);
 		})
 		.error(function(data) {
 			console.log('Error: ' + data);
 		});
 		
 		
-
+/*
 	// when submitting the add form, send the text to the node API
 	$scope.createTodo = function() {
 		$http.post('/api/todos', $scope.formData)
@@ -39,6 +42,7 @@ function mainController($scope, $http) {
 				console.log('Error: ' + data);
 			});
 	};
+*/
 
 	// delete a todo after checking it
 	$scope.deleteTodo = function(id) {
